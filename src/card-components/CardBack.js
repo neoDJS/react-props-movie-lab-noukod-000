0c54e6071dc254d338f4ace37c32a34d1d81734c
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-25 20:21:29 +0000

    Automatically backed up by Learn

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -19,7 +19,7 @@ export default class CardBack extends Component {
       <div className="card-back">
         <h3 className="title">{this.props.title}</h3>
         <span />
-        { /* your rating element should go here -- you can invoke methods within JSX, à la: this.myMethod() */ }
+        { this.generateRatingElement()/* your rating element should go here -- you can invoke methods within JSX, à la: this.myMethod() */ }
         <span />
         <h5 className="genres">{this.props.genres.join(', ')}</h5>
       </div>
